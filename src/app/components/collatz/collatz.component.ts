068e254ebf742f8c0ecf527d999ddfd82f4e2277
javierpantoja1998 <90837450+javierpantoja1998@users.noreply.github.com>
2022-11-14 16:43:19 +0100

    terminado

--- src/app/components/collatz/collatz.component.ts
+++ src/app/components/collatz/collatz.component.ts
@@ -15,12 +15,31 @@ export class CollatzComponent implements OnInit {
   constructor(private _activedRouter: ActivatedRoute, private _router: Router) {
     this.numeros = new Array<number>();
   }
+  doCollatz(): void {
+    this.numeros = new Array<number>();
+        //Hacemos el while para calcular el metodo Collatz
+            while(this.numero != 1){
+                if(this.numero % 2 == 0) {
+                  this.numero /= 2
+                }else{
+                  this.numero = this.numero * 3 + 1
+                }
+                //Metemos los numeros del collatz al array numeros
+                this.numeros.push(this.numero);
+                console.log(this.numeros)
+            }
 
+  }
   ngOnInit(): void {
     this._activedRouter.params.subscribe((parametros: Params) => {
 
         //Hacemos el while para calcular el metodo Collatz
         console.log(parametros['numero'])
+        this.numero = parseInt(parametros['numero']);
+
+        this.doCollatz();
+
+
 
     });
   }
